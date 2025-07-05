acfc066525a41e77644fa397acfb8860a410848f
JP-Ellis <josh@jpellis.me>
2025-07-06 09:41:53 +1000

    chore: add groundwork

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+//! # Rust client for Amber Electric's API.
+
+#[cfg(test)]
+mod tests {
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(1_i32 + 1_i32, 2_i32);
+    }
+}
