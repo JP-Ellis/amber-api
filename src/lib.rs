f97f53462c14aba663febf8460f5e219aca85aaa
JP-Ellis <josh@jpellis.me>
2025-07-06 20:52:36 +1000

    chore: remove dummy test
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,3 @@
 //! # Rust client for Amber Electric's API.
 
 pub mod models;
-
-#[cfg(test)]
-mod tests {
-    use pretty_assertions::assert_eq;
-
-    #[test]
-    fn it_works() {
-        assert_eq!(1_i32 + 1_i32, 2_i32);
-    }
-}
